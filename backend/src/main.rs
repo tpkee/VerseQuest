bb5e008a78db7fe64fd7c1d907ed8d837264872b
tpkee <89136677+tpkee@users.noreply.github.com>
2023-05-28 16:06:51 +0200

    feat: added dockerfile

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,61 +1,26 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
+    routing::{get},
     Json, Router,
 };
-use serde::{Deserialize, Serialize};
-use tower_http::cors::{CorsLayer, Any};
+
+const PORT: u16 = 8080;
+const ADDR: &str = "0.0.0.0";
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
     tracing_subscriber::fmt::init();
 
-    let cors = CorsLayer::new().allow_origin(Any); // todo: update with a real list of cors
-
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/", get(root));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&format!("{ADDR}:{PORT}").parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+    tracing::debug!("listening on {PORT}");
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
